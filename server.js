d53bfbe9af27e4e1ef48d269fb554134b750b5e4
Sergey Ostrovsky <sostrovs@gmail.com>
2016-03-14 12:45:06 -0700

    Add front-end form to submit message

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var PORT = process.env.PORT || 3000
 var express = require('express')
-var app =  express()
+var app = express()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
 
@@ -8,10 +8,9 @@ app.use(express.static(__dirname + '/public'))
 
 io.on('connection', function(socket) {
 	console.log('User conncted via socket.io!')
-	
+
 	socket.on('message', function(message) {
 		console.log('Message received: ' + message.text)
-
 		socket.broadcast.emit('message', message)
 	})
 
